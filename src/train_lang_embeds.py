ab181b8290d708b8ae6f7ed1c36b5d40d5ca28f0
Diana Abagyan <36429533+syntaxshill@users.noreply.github.com>
2021-04-06 14:26:10 -0400

    added linguistic distance and language embedding generation

---
+++ src/train_lang_embeds.py
@@ -0,0 +1,54 @@
+'''Written by Diana Abagyan'''
+
+import pickle
+import json
+import torch
+import matplotlib.pyplot as plt
+
+
+#epochs = 20
+threshold = .005
+with open("../dictionaries/language_to_index.json") as f:
+    lang_to_ind = json.load(f)
+
+
+def loss_fn(lang_embeds, true_dists):
+    loss_sum = 0
+    for l1, l2, dist in true_dists:
+        v1 = lang_embeds[lang_to_ind[l1]]
+        v2 = lang_embeds[lang_to_ind[l2]]
+        loss_sum += abs(dist - torch.linalg.norm(v1 - v2, 2))
+
+    return loss_sum
+
+
+if __name__ == '__main__':
+    with open("../linguistic_distance/leven_dist_min.pkl", 'rb') as f:
+        true_dists = pickle.load(f)
+
+    # initialize embeddings
+    lang_embeds = [torch.rand(5, requires_grad=True) for _ in range(10)]
+
+    # optimize
+    optimizer = torch.optim.SGD(lang_embeds, lr=0.01)
+    loss_record = []
+    optimize = True
+    epochs = 0
+    while optimize:
+        epochs += 1
+        optimizer.zero_grad()
+        loss = loss_fn(lang_embeds, true_dists)
+        loss_record.append(loss.item())
+        loss.backward()
+        optimizer.step()
+
+        if len(loss_record) > 2:    # stop if difference between epochs is below threshold
+            if (loss_record[-2] - loss_record[-1]) < threshold:
+                optimize = False
+
+    # plot loss
+    plt.plot([i for i in range(epochs)], loss_record)
+    plt.show()
+
+    # save embeddings
+    torch.save(lang_embeds, "../linguistic_distance/linguistic_distance.pt")
